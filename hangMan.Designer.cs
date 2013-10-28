a5b6c259a9f9c466db15f8d493954c354463213d
cziner21 <cziner.adam@gmail.com>
2013-10-28 10:44:11 +0100

    Version 1.0
    Akaaztófa játék 1.0-ás verziója

---
+++ hangMan.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace aksztofa
+{
+    partial class HangMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HangMan));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.quit = new System.Windows.Forms.ToolStripMenuItem();
+            this.infóToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.categoryBox = new System.Windows.Forms.ComboBox();
+            this.chooseCateory = new System.Windows.Forms.Label();
+            this.selectBtn = new System.Windows.Forms.Button();
+            this.guessLetter = new System.Windows.Forms.TextBox();
+            this.guessLabel = new System.Windows.Forms.Label();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.wrongLt = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Aqua;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.infóToolStripMenuItem,
+            this.helpToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGame,
+            this.quit});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.ShortcutKeyDisplayString = "_";
+            this.helpToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F)));
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.helpToolStripMenuItem.Text = "File";
+            // 
+            // newGame
+            // 
+            this.newGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.newGame.Image = global::aksztofa.Properties.Resources.tux;
+            this.newGame.Name = "newGame";
+            this.newGame.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.U)));
+            this.newGame.Size = new System.Drawing.Size(152, 22);
+            this.newGame.Text = "Új játék";
+            this.newGame.Click += new System.EventHandler(this.newGame_Click);
+            // 
+            // quit
+            // 
+            this.quit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.quit.Image = global::aksztofa.Properties.Resources.cross;
+            this.quit.Name = "quit";
+            this.quit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.K)));
+            this.quit.Size = new System.Drawing.Size(152, 22);
+            this.quit.Text = "Kilépés";
+            this.quit.Click += new System.EventHandler(this.quit_Click);
+            // 
+            // infóToolStripMenuItem
+            // 
+            this.infóToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.infóToolStripMenuItem.Name = "infóToolStripMenuItem";
+            this.infóToolStripMenuItem.ShortcutKeyDisplayString = "_";
+            this.infóToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.I)));
+            this.infóToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.infóToolStripMenuItem.Text = "Info";
+            this.infóToolStripMenuItem.Click += new System.EventHandler(this.infóToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.ShortcutKeyDisplayString = "_";
+            this.helpToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.H)));
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem1.Text = "Help";
+            this.helpToolStripMenuItem1.Click += new System.EventHandler(this.helpToolStripMenuItem1_Click);
+            // 
+            // categoryBox
+            // 
+            this.categoryBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.categoryBox.FormattingEnabled = true;
+            this.categoryBox.Location = new System.Drawing.Point(12, 54);
+            this.categoryBox.Name = "categoryBox";
+            this.categoryBox.Size = new System.Drawing.Size(157, 21);
+            this.categoryBox.TabIndex = 2;
+            // 
+            // chooseCateory
+            // 
+            this.chooseCateory.AutoSize = true;
+            this.chooseCateory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.chooseCateory.Location = new System.Drawing.Point(12, 28);
+            this.chooseCateory.Name = "chooseCateory";
+            this.chooseCateory.Size = new System.Drawing.Size(128, 17);
+            this.chooseCateory.TabIndex = 3;
+            this.chooseCateory.Text = "Válassz kategóriát:";
+            // 
+            // selectBtn
+            // 
+            this.selectBtn.BackColor = System.Drawing.Color.Silver;
+            this.selectBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.selectBtn.ForeColor = System.Drawing.Color.Green;
+            this.selectBtn.Location = new System.Drawing.Point(12, 90);
+            this.selectBtn.Name = "selectBtn";
+            this.selectBtn.Size = new System.Drawing.Size(75, 23);
+            this.selectBtn.TabIndex = 4;
+            this.selectBtn.Text = "Kiválaszt";
+            this.selectBtn.UseVisualStyleBackColor = false;
+            this.selectBtn.Click += new System.EventHandler(this.selectBtn_Click);
+            // 
+            // guessLetter
+            // 
+            this.guessLetter.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.guessLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.guessLetter.ForeColor = System.Drawing.Color.Green;
+            this.guessLetter.Location = new System.Drawing.Point(275, 94);
+            this.guessLetter.MaxLength = 1;
+            this.guessLetter.Name = "guessLetter";
+            this.guessLetter.Size = new System.Drawing.Size(37, 20);
+            this.guessLetter.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.guessLetter, "Csak a magyar ábécé betűi írhatók be, egyszerre csak 1 karakter.\r\n(a,á,b,c,d,e,é," +
+                    "f,g,h,i,í,stb...)\r\n");
+            // 
+            // guessLabel
+            // 
+            this.guessLabel.AutoSize = true;
+            this.guessLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.guessLabel.Location = new System.Drawing.Point(184, 100);
+            this.guessLabel.Name = "guessLabel";
+            this.guessLabel.Size = new System.Drawing.Size(80, 13);
+            this.guessLabel.TabIndex = 6;
+            this.guessLabel.Text = "Írj be egy betűt:";
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.BackColor = System.Drawing.Color.Silver;
+            this.sendBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sendBtn.ForeColor = System.Drawing.Color.Green;
+            this.sendBtn.Location = new System.Drawing.Point(327, 91);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendBtn.TabIndex = 7;
+            this.sendBtn.Text = "Elküld";
+            this.sendBtn.UseVisualStyleBackColor = false;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::aksztofa.Properties.Resources.akasztofadefault;
+            this.pictureBox1.Location = new System.Drawing.Point(126, 142);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(296, 282);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // wrongLt
+            // 
+            this.wrongLt.AutoSize = true;
+            this.wrongLt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.wrongLt.Location = new System.Drawing.Point(368, 91);
+            this.wrongLt.Name = "wrongLt";
+            this.wrongLt.Size = new System.Drawing.Size(81, 13);
+            this.wrongLt.TabIndex = 8;
+            this.wrongLt.Text = "Rossz betűk:";
+            this.wrongLt.Visible = false;
+            // 
+            // HangMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(584, 436);
+            this.Controls.Add(this.wrongLt);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.guessLabel);
+            this.Controls.Add(this.guessLetter);
+            this.Controls.Add(this.selectBtn);
+            this.Controls.Add(this.chooseCateory);
+            this.Controls.Add(this.categoryBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "HangMan";
+            this.Text = "Akasztófa© v1.0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infóToolStripMenuItem;
+        private System.Windows.Forms.ComboBox categoryBox;
+        private System.Windows.Forms.Label chooseCateory;
+        private System.Windows.Forms.Button selectBtn;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+        private System.Windows.Forms.TextBox guessLetter;
+        private System.Windows.Forms.Label guessLabel;
+        private System.Windows.Forms.ToolStripMenuItem newGame;
+        private System.Windows.Forms.ToolStripMenuItem quit;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Label wrongLt;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ hangMan.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace aksztofa
+{
+
+
+    public partial class HangMan : Form
+    {
+        //beírt karakter
+        public string guessChar = "";
+        //Dinamikus címkék
+        public List<Label> labels = new List<Label>();
+        //választott szó (choosenWord, csak már sok volt a choose, és a kavarodás elkerülése miatt rövidítettem)
+        public string cWord;
+        //rossz betűk tárolására
+        public List<string> wrongLetters = new List<string>();
+        //ebbe tárolom a már kitalált betűket
+        public List<string> tmp = new List<string>();
+        
+        /// <summary>
+        /// Konstruktor, kezdő felület beállítása
+        /// </summary>
+        public HangMan() {
+            InitializeComponent();
+            guessLabel.Visible = false;
+            guessLetter.Visible = false;
+            sendBtn.Visible = false;
+            categoryBox.SelectedIndex = -1;
+
+            categoryBox.Items.Add("Állat");
+            categoryBox.Items.Add("Növény");
+            categoryBox.Items.Add("Keresztnév");
+            categoryBox.Items.Add("Tárgy");
+        }
+
+        /// <summary>
+        /// miután kiválasztottuk a megfelelő kategóriát(ha nem választunk, a 0-át választja)
+        /// kialakítja a felületet és meghívja a Read függvényt
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void selectBtn_Click(object sender, EventArgs e) {
+
+            //felesleges gombok, comboboxok, labelek eltüntetése
+            chooseCateory.Visible = false;
+            selectBtn.Visible = false;
+            categoryBox.Visible = false;
+
+            int index;
+            if (categoryBox.SelectedIndex >= 0 && categoryBox.SelectedIndex <= 3)
+                index = categoryBox.SelectedIndex;
+            else
+                index = 0;
+            //kiválasztott kategórából való beolvasás
+            Read(index);
+
+            //új elemek megjelenítése
+            guessLabel.Visible = true;
+            guessLetter.Visible = true;
+            sendBtn.Visible = true;
+            guessLabel.Location = new Point(10, 100);
+            guessLetter.Location = new Point(101, 94);
+            sendBtn.Location = new Point(160, 93);
+        }
+
+        /// <summary>
+        /// a kategóriának megfelelő text fileból beolvas mindent, randomra választ egy szót, majd meghívj rá
+        /// a címkegeneráló függvényt
+        /// </summary>
+        /// <param name="category"></param>
+        public void Read(int category) {
+            string[] text;
+            Random rnd = new Random();
+
+            if (category == 0) {
+                text = System.IO.File.ReadAllLines(@"D:/Work/Programok/bevgraf/allat.txt");
+                int lines = text.Length;
+                int chosen = rnd.Next(0, lines);
+                char[] letters = text[chosen].ToUpper().ToCharArray();
+                cWord = text[chosen];
+                //Dinamikusan annyi címkét generál, ammenyi betű van a szóban               
+                generateLabels(letters);              
+            }           
+            if (category == 1) {
+                text = System.IO.File.ReadAllLines(@"D:/Work/Programok/bevgraf/noveny.txt");
+                int lines = text.Length;
+                int chosen = rnd.Next(0, lines);
+                char[] letters = text[chosen].ToUpper().ToCharArray();
+                cWord = text[chosen];
+                //Dinamikusan annyi címkét generál, ammenyi betű van a szóban               
+                generateLabels(letters);
+            }
+            if (category == 2) {
+                text = System.IO.File.ReadAllLines(@"D:/Work/Programok/bevgraf/keresztnev.txt");
+                int lines = text.Length;
+                int chosen = rnd.Next(0, lines);
+                char[] letters = text[chosen].ToUpper().ToCharArray();
+                cWord = text[chosen];
+                //Dinamikusan annyi címkét generál, ammenyi betű van a szóban               
+                generateLabels(letters);
+            }
+            if (category == 3) {
+                text = System.IO.File.ReadAllLines(@"D:/Work/Programok/bevgraf/targy.txt");
+                int lines = text.Length;
+                int chosen = rnd.Next(0, lines);
+                char[] letters = text[chosen].ToUpper().ToCharArray();
+                cWord = text[chosen];
+                //Dinamikusan annyi címkét generál, ammenyi betű van a szóban               
+                generateLabels(letters);
+            }
+            
+        }
+
+        /// <summary>
+        /// Ha jó betűt írunk, akkor kicseréli a "_" jeleket a megfelelő betűkre
+        /// ha rosszat, hozzáadogatja  a wrognLetters listához a rosszakat
+        /// </summary>
+        /// <param name="labels"></param>
+        /// <param name="chosedWord"></param>
+        /// <param name="gc"></param>
+        public void controlGame(List<Label> labels, string chosedWord, string gc) {
+                
+                if (chosedWord.Contains(gc)) {
+                    
+                    for (int j = 0; j < chosedWord.Length; j++) {
+                        if (chosedWord[j] == gc[0]) {
+                            labels[j].ForeColor = Color.Green;
+                            labels[j].Font = new Font(labels[j].Font, FontStyle.Bold);
+                            labels[j].Text = gc.ToString();
+                            tmp.Add(gc);
+                        }                        
+                    }                    
+                }
+                else{
+                    if (!wrongLetters.Contains(gc))
+                        wrongLetters.Add(gc);
+                    else
+                        MessageBox.Show("Ezt a betűt, már beírtad egyszer!","Duplikált betű!");
+                }
+                //ha a kitalált betűk hossza megegyezik
+                //a kitalálandó szó hosszával, akkor nyertünk
+                if (tmp.Count == chosedWord.Length) {
+                    guessLetter.Enabled = false;
+                    MessageBox.Show("Gratulálok, nyertél!","Nyertél!");
+                }
+
+        }
+
+        /// <summary>
+        /// Ez a metódus dinamikusan generál címkéket
+        /// a paraméterként kapott szó hosszának megfelelő 
+        /// darabszámmal
+        /// </summary>
+        /// <param name="letters">karakter tömb</param>
+        public void generateLabels(char[] letters) {
+            //20 pixel távra lesznek egymástól a címkék
+            int distance = 120;
+            for (int i = 0; i < letters.Length; i++) {
+                Label newLabel = new Label();
+                //Label NewLabel = new Label();
+                newLabel.Name = "chosenLetter" + i;
+                newLabel.Text = "_";
+                newLabel.Location = new Point(distance, 35);
+                newLabel.Visible = true;
+                labels.Add(newLabel);
+                distance += 20;
+            }
+
+            foreach (Label l in labels) {
+                this.Controls.Add(l);
+                l.BringToFront();
+            }                
+        }
+
+        /// <summary>
+        /// Bezárja a formot
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void quit_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Új játék indítása
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void newGame_Click(object sender, EventArgs e) {
+
+            //takarítás
+            this.Controls.Clear();
+            this.labels.Clear();
+            this.wrongLetters.Clear();
+            this.tmp.Clear();
+            this.InitializeComponent();
+
+            guessLabel.Visible = false;
+            guessLetter.Visible = false;
+            sendBtn.Visible = false;
+            categoryBox.SelectedIndex = -1;
+
+            categoryBox.Items.Add("Állat");
+            categoryBox.Items.Add("Növény");
+            categoryBox.Items.Add("Keresztnév");
+            categoryBox.Items.Add("Tárgy");            
+        }
+
+        /// <summary>
+        /// Ellenőrzés után meghívja a controlGame függvényt
+        /// ha rossz betűt írunk, akkor elkezdi rajzolni a fát
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void sendBtn_Click(object sender, EventArgs e) {
+            string chosedWord = cWord.ToUpper();
+            
+            guessChar = guessLetter.Text.ToUpper();
+            if (Regex.IsMatch(guessChar, "^[A-Za-záÁéÉíÍóÓöÖüÜőŐúÚűŰ]$"))
+                controlGame(labels, chosedWord, guessChar);
+            else
+                MessageBox.Show("Csak betűket írhatsz be!");
+            
+            //rossz betűk megjelennek
+            wrongLt.Visible = true;            
+            int q = 0;
+            for (int i = 0; i < wrongLetters.Count; i++) {
+                Label wLabel = new Label();
+                wLabel.Name = "wl" + i;
+                wLabel.Text = wrongLetters[i];
+                wLabel.ForeColor = Color.Red;
+                wLabel.Location = new Point(324 + q, 120);
+                this.Controls.Add(wLabel);
+                wLabel.BringToFront();
+                q += 20;
+            }
+            
+            //akasztófa állásait kirajzolja
+            if (wrongLetters.Count == 1)
+                pictureBox1.Image = Properties.Resources.akasztofa01;
+            else if (wrongLetters.Count == 2)
+                pictureBox1.Image = Properties.Resources.akasztofa02;
+            else if (wrongLetters.Count == 3)
+                pictureBox1.Image = Properties.Resources.akasztofa03;
+            else if (wrongLetters.Count == 4)
+                pictureBox1.Image = Properties.Resources.akasztofa04;
+            else if (wrongLetters.Count == 5)
+                pictureBox1.Image = Properties.Resources.akasztofa05;
+            else if (wrongLetters.Count == 6)
+                pictureBox1.Image = Properties.Resources.akasztofa06;
+            else if (wrongLetters.Count == 7)
+                pictureBox1.Image = Properties.Resources.akasztofa07;
+            else if (wrongLetters.Count == 8)
+                pictureBox1.Image = Properties.Resources.akasztofa08;
+            else if (wrongLetters.Count == 9)
+                pictureBox1.Image = Properties.Resources.akasztofa09;
+            else if (wrongLetters.Count == 10)
+                pictureBox1.Image = Properties.Resources.akasztofa10;
+            else if (wrongLetters.Count == 11) {
+                pictureBox1.Image = Properties.Resources.akasztofa11;
+                guessLetter.Enabled = false;
+                MessageBox.Show("Meghaltál!\n"+
+                                "-------------\n"+
+                                "Ez volt a gonolt szó:\n\n"+
+                                cWord.ToUpper(),"Game Over!");                
+            }
+            //minden beküldött betű után kipucolja az input mezőt
+            guessLetter.Clear();
+        }
+
+        /// <summary>
+        /// infó menüpont
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void infóToolStripMenuItem_Click(object sender, EventArgs e) {
+            MessageBox.Show("Készítette: Cziner Ádám\n"+
+                            "v1.0 verzió\n", "Akasztófa© v1.0");
+        }
+
+        /// <summary>
+        /// help menü
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void helpToolStripMenuItem1_Click(object sender, EventArgs e) {
+            MessageBox.Show("Akasztófa - Súgó\n" +
+                            "------------------\n\n" +
+                            "A játék elején választani kell egy kategóriát, amely alapján\n" +
+                            "a gép véletlenszerűen választ egy szót, a kategóriának megfelelő txt fileból.\n" +
+                            "Ezután megjelenik egy beviteli mező, amibe a magyar ábécé karaktereit beírva\n" +
+                            "találgathatunk, hogy mi lehet a szó. Összesen 11-et hibázhatunk,\n" +
+                            "ha ezt a határt túllépjük, vége a játéknak.", "Súgó");
+        }
+
+        
+       
+    }
+}
